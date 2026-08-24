ec15af687f04a50874e2fb2f6e2c045dcefa16d8
Alex <1inboxna@gmail.com>
2026-08-24 14:21:22 +0300

    Initial commit for portfolio

---
+++ src/scripts/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// прибираю скрол під менюшкою на мобільном
+
+// window.addEventListener('hashchange', () => {
+//   const isMenuOpen = window.location.hash === '#menu';
+
+//   document.body.style.overflow = isMenuOpen ? 'hidden' : '';
+// });
+
+// номер телефона при наведенні
+
+(() => {
+  const phoneNum = document.getElementById('phone-num');
+  const phone = document.getElementById('phone');
+
+  phone.addEventListener('mouseenter', () => {
+    phoneNum.classList.remove('hidden');
+  });
+
+  phone.addEventListener('mouseleave', () => {
+    phoneNum.classList.add('hidden');
+  });
+})();
+
+// додаю при прокруткі фон для шапки
+
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.header');
+
+  if (window.scrollY > 10) {
+    header.classList.add('header--scrolled');
+  } else {
+    header.classList.remove('header--scrolled');
+  }
+});
